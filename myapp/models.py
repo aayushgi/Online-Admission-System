c3761c2263509bd44ae57887b1f6eb23436734ec
ayushgi <gitayush30@gmail.com>
2026-07-15 13:11:01 +0530

    django admin

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class login(models.Model):
+    id=models.IntegerField(primary_key=True,auto_created=True)
+    username=models.CharField(max_length=100)
+    password=models.CharField(max_length=16)
+    status=models.CharField(max_length=20)
+
+class session(models.Model):
+    id=models.IntegerField(primary_key=True,auto_created=True)
+    session_name=models.CharField(max_length=20)
+    create_at=models.TimeField()
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import*
+from datetime import datetime
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    return render(request, 'student/home.html')
+
+def adminlogin(request):
+    return render(request, 'admin/admin_login.html')
+
+def loginsave(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=login.objects.filter(username=username,password=password).first()
+        if user:
+            if user.status=="Active":
+                request.session['adminid']=username
+                return redirect('dashboard')
+        else:
+            return redirect('adminlogin')
+def dashboard(request):
+    return render(request, 'admin/dashboard.html')
+
+
+def admindash(request):
+    return render(request, 'admin/adminlayout.html')
+
+
+def addsession(request):
+    if request.method=="POST":
+        session_name=request.POST.get('session_name')
+        create_at=datetime.now()
+        ab=session(session_name=session_name,create_at=create_at)
+        ab.save()
+        messages.success(request, 'Session Added Successfully')
+        return redirect('addsession')  
+    return render(request, 'admin/addsession.html')
