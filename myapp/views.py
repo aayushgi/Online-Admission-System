915bf1a3940aaad76c299e2628ab26d4d64a1659
ayushgi <gitayush30@gmail.com>
2026-07-18 20:40:58 +0530

    django admin

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from .models import*
 from datetime import datetime
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def home(request):
@@ -54,7 +56,7 @@ def addsession(request):
 def deletesession(request,id):
     ab=session.objects.get(id=id)
     ab.delete()
-    messages.success(request,'session Deleted Successfully')
+    messages.info(request,'session Deleted Successfully')
     return redirect('showsession')
 
 def editcourse(request, id):
@@ -71,14 +73,14 @@ def updatecourse(request, id):
         course.duration = request.POST.get('duration')
         course.fees = request.POST.get('fees')
         course.save()
-        messages.success(request, 'Course Updated Successfully')
+        messages.info(request, 'Course Updated Successfully')
         return redirect('showcourse')
     return render(request, 'admin/editcourse.html', {'course': course})
 
 def deletecourse(request, id):
     course = tbl_course.objects.get(id=id)
     course.delete()
-    messages.success(request, 'Course Deleted Successfully')
+    messages.info(request, 'Course Deleted Successfully')
     return redirect('showcourse')
 
 
@@ -126,6 +128,60 @@ def showcourse(request):
 def logout(request):
     request.session.flush()
     return redirect('adminlogin')
-
 def addstudent(request):
+    if request.method == "POST":
+
+        name = request.POST.get('name')
+        emailaddress = request.POST.get('emailaddress')
+        password = request.POST.get('password')
+
+        tbl_student.objects.create(
+            name=name,
+            emailaddress=emailaddress,
+            password=password,
+            contact_no=request.POST.get('contact_no'),
+            gender=request.POST.get('gender'),
+            dob=request.POST.get('dob'),
+        )
+
+        message = f"""
+Dear {name},
+
+🎉 Congratulations!
+
+Your registration has been completed successfully on the Biotech Park Admission Portal.
+
+You can now log in using the credentials below:
+
+---------------------------------------
+User ID / Email : {emailaddress}
+Password        : {password}
+---------------------------------------
+
+Next Steps:
+1. Login to the Admission Portal.
+2. Complete your Basic Information.
+3. Upload all required documents.
+4. Wait for document verification by the Admin.
+5. After verification, pay your admission fee.
+6. Once the payment is successful, your course will be allotted.
+
+Please keep this email safe for future login and admission-related communication.
+
+Regards,
+Biotech Park Admission Team
+"""
+        
+
+        send_mail(
+            subject="Registration Successful",
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[emailaddress],
+             fail_silently=False
+        )
+
+        messages.success(request, "Student Added Successfully")
+        return redirect('addstudent')
+
     return render(request, 'admin/adduser.html')
--- oas/settings.py
+++ oas/settings.py
@@ -116,3 +116,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+#this block is for email integration 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'mrayushsaxena09@gmail.com'
+EMAIL_HOST_PASSWORD = 'hdwu odrx rmjy xdlt'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+#-------------------------------------------------------------
